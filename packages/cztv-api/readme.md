1d30a8c506b5c2b145f8c5016774fabf19390236
zsj <zsj5@foxmail.com>
2022-08-18 11:22:15 +0800

    完成客户端当前接口联调，完善文档和注释

--- packages/cztv-api/readme.md
+++ packages/cztv-api/readme.md
@@ -2,7 +2,7 @@
 
 ## 前言
 
-### 说明
+### Description
 
 蓝TV客户端jsBridge方法封装
 
@@ -12,29 +12,143 @@
 
 ```bash
 yarn add @newblue/cztv-api
+```
+or
+```bash
 npm install @newblue/cztv-api --registry=http://10.200.37.236:4873/
 ```
 
-### Usage
+### 🔨 Usage
 
 ```js
-    import CztvApi  from '@newblue/cztv-api';
+import CztvApi  from '@newblue/cztv-api'
+	
+const shareData = {
+	url: "http://mhudong.cztv.com/260366",
+	title: "123",
+	content: "213",
+	img: "https://ohudong.cztv.com/1/260366/images/qrcode.png"
+}
+const param = {
+	debug: true, // 显示调试信息
+	...shareData
+}
+const cztvApi = new CztvApi(param) // 可不传参
 ```
 
-## Publish
+### 🚀 Examples
 
-```bash
-npm run pub
-```
-
-## 技术
-
-### 技术栈：
-
-rollup/ts/...
-
-### 🔨 示例
-
+显示和隐藏分享按钮
 ```js
-       
+cztvApi.showShareButton('true')
+```
+
+修改分享参数
+```js
+cztvApi.changeShareInfo(shareData)
+```
+
+调起客户端分享
+```js
+cztvApi.share(shareData)
+```
+
+获取版本号
+```js
+cztvApi.version()
+```
+
+调起摇一摇音效
+```js
+cztvApi.playSound()
+```
+
+打开点播页
+```js
+cztvApi.vodPlay({
+	videoId: '717862',
+	videoTitle: '《2019中国好声音》第15期：鸟巢总决赛  四大战队严正以待',
+	videoShareUrl: 'https://tv.cztv.com/vplay/717862.html',
+	videoImage: 'http://i05.cztv.com/cztv/vms/2019/10/07/8f00f140b7894ac1813fc82dd78c7519/9_640_400.jpg',
+	videoAblumId: '1933',
+	videoChannelId: '11'
+})
+```
+
+打开会员页
+```js
+cztvApi.pushVipPage()
+```
+
+唤起登录页
+```js
+cztvApi.login()
+```
+
+获取用户信息，未登录会唤起登录页（老接口）
+```js
+cztvApi.getUserInfo()
+```
+
+仅获取用户信息（新接口）
+```js
+cztvApi.userInfo()
+```
+
+修改标题
+```js
+cztvApi.refreshTitle('new Title')
+```
+
+分享平台配置，配置右上角调起的分享面板中的图标和方法，后续可扩展
+```js
+cztvApi.addSharePlatform({
+    "shortcut":{
+        "shortLabel": "测试-画展",
+        "icon": "https://ohudong.cztv.com/1/264275/img/01.903b677c.png",
+        "url": "http://mhudong.cztv.com/264275",
+        "isPinShortcutSupported": true
+    }
+})
+```
+
+查询客户端是否支持指定的JS方法
+```js
+cztvApi.checkJsApi({
+	apiList: [
+		"CZTVJsdk.showShareButton",
+		"CZTVJsdk.share"
+	]
+})
+```
+
+图片缩放预览
+```js
+cztvApi.previewImage({
+	currentIndex: 0,
+	imgeData: "https://ohudong.cztv.com/1/264275/img/02.82c9adb5.png,https://ohudong.cztv.com/1/264275/img/03.ffb577cb.png" // 网络地址，逗号分隔
+})
+```
+
+获取网络状态
+```js
+cztvApi.getNetworkType()
+```
+
+调起app页面
+```js
+cztvApi.getNetworkType({
+	resource_type: 0, //跳转类型
+	resource_body: "101" //页面加载所用到的id
+})
+```
+
+配置胶囊样式UI
+```js
+cztvApi.getNetworkType({
+    showCapsule: true,
+    capsuleBgColor: '#ffffff',
+    capsuleButtonColor: '#000000',
+    icon: 'https://ohudong.cztv.com/1/261597/images/tvLogo.png'
+})
 ```
--- packages/cztv-api/src/index.ts
+++ packages/cztv-api/src/index.ts
@@ -1,3 +1,5 @@
+const { version } = require('../package.json')
+
 /**
  * 配合 IOS 使用时的初始化方法
  */
@@ -45,20 +47,26 @@ function androidFunction(callback) {
 
 const setupWebViewJavascriptBridge = isAndroid ? androidFunction : iosFunction
 
-/*
-  showShareButton:(string) 'true'/'false'，控制app右上角分享是否显示；
-  shareData:(object) 分享参数;
+/**
+ * @description: 创建实例，可预设分享参数，可显示和隐藏调试信息
+ * @param {Object} param 对象解构，剩余参数作为预设分享参数 可选
+ * {Boolean} debug 显示和隐藏调试信息 可选
+ * {Object} shareData 预设分享参数 可选
+ * {
       "url": 'http://mhudong.cztv.com/260366', //分享的网址链接
       "title": "123", //分享的标题
       "content": "213", // 分享的文字
       "img": "https://ohudong.cztv.com/1/260366/images/qrcode.png", // 分享的图片Url
-*/
-function CztvApi(shareData) {
-  console.log('v1.0.0')
+    }
+ * @return {*}
+ */
+function CztvApi({ debug = false, ...shareData } = {}) {
+  console.log('CztvApi ' + version)
   if (shareData) {
     this.shareData = shareData
   }
-  // 客户端初始化存在延迟，过早调用可能导致失败
+
+  this.debug = debug
 }
 
 /* 设置shareData数据   分享的信息
@@ -78,54 +86,73 @@ function CztvApi(shareData) {
 // }
 
 /**
- * 显示和隐藏分享按钮
- * @param {*} showShareButton
+ * @description: 显示和隐藏分享按钮
+ * @param {String} showShareButton 字符串类型的布尔值,'true'/'false'
+ * @return {*}
  */
 CztvApi.prototype.showShareButton = function (showShareButton = 'true') {
-  console.log('showShareButton:', showShareButton)
+  if (this.debug) console.log('showShareButton:', showShareButton)
+
   setupWebViewJavascriptBridge(function (bridge) {
     bridge.callHandler('CZTVJsdk.showShareButton', showShareButton)
   })
 }
 
 /**
- * 修改分享参数
- * @param {*} shareData
- */
-CztvApi.prototype.changeShareInfo = function (shareData = this.shareData) {
-  console.log('changeShareInfo:', shareData)
-  shareData != null && setupWebViewJavascriptBridge(function (bridge) {
-    bridge.callHandler('CZTVJsdk.changeShareInfo', shareData, function (res) {
-      console.log('设置分享参数:', res)
-    })
-  })
-}
-
-/*
-  h5调起客户端分享
-  shareData:(object) 分享参数;
+ * @description: 修改分享参数
+ * @param {Object} shareData
+ *  {
       "url": 'http://mhudong.cztv.com/260366', //分享的网址链接
       "title": "123", //分享的标题
       "content": "213", // 分享的文字
       "img": "https://ohudong.cztv.com/1/260366/images/qrcode.png", // 分享的图片Url
-*/
-CztvApi.prototype.shareCZTVApp = function (shareData = this.shareData) {
-  console.log('shareCZTVApp:', shareData)
+    }
+ * @return {*}
+ */
+CztvApi.prototype.changeShareInfo = function (shareData = this.shareData) {
+  const debug = this.debug
+  if (debug) console.log('changeShareInfo:', shareData)
+
   shareData != null && setupWebViewJavascriptBridge(function (bridge) {
-    bridge.callHandler('CZTVJsdk.share', shareData, function (res) {
-      console.log('调起客户端分享：', res)
+    bridge.callHandler('CZTVJsdk.changeShareInfo', shareData, function (res) {
+      if (debug) console.log('设置分享参数:', res)
     })
   })
 }
 
-/*
-    获取版本号
-*/
+/**
+ * @description: h5调起客户端分享
+ * @param {Object} shareData
+ *  {
+      "url": 'http://mhudong.cztv.com/260366', //分享的网址链接
+      "title": "123", //分享的标题
+      "content": "213", // 分享的文字
+      "img": "https://ohudong.cztv.com/1/260366/images/qrcode.png", // 分享的图片Url
+    }
+ * @return {*}
+ */
+CztvApi.prototype.share = function (shareData = this.shareData) {
+  const debug = this.debug
+  if (debug) console.log('share:', shareData)
+
+  shareData != null && setupWebViewJavascriptBridge(function (bridge) {
+    bridge.callHandler('CZTVJsdk.share', shareData, function (res) {
+      if (debug) console.log('调起客户端分享：', res)
+    })
+  })
+}
+
+/**
+ * @description: 获取版本号
+ * @param {*} resCallback
+ * @return {*}
+ */
 CztvApi.prototype.version = function (resCallback) {
+  if (this.debug) console.log('version')
+
   setupWebViewJavascriptBridge(function (bridge) {
-    console.log('version')
     bridge.callHandler('CZTVJsdk.version', null, function (res) {
-      // console.log(res);
+      // console.log(res)
       if (resCallback) {
         resCallback(res)
       }
@@ -133,43 +160,55 @@ CztvApi.prototype.version = function (resCallback) {
   })
 }
 
-/*
-    调起客户端 摇一摇音效
-*/
+/**
+ * @description: 调起摇一摇音效
+ * @return {*}
+ */
 CztvApi.prototype.playSound = function () {
+  const debug = this.debug
+  if (debug) console.log('playSound')
+
   setupWebViewJavascriptBridge(function (bridge) {
-    console.log('playSound')
     bridge.callHandler('CZTVJsdk.playSound', null, function (res) {
-      console.log(res)
+      if (debug) console.log('playSound:', res)
     })
   })
 }
 
-/*
-    打开点播页
-    playOptions:(object) 视频参数;
-        videoId: '717862',
-        videoTitle: '《2019中国好声音》第15期：鸟巢总决赛  四大战队严正以待',
-        videoShareUrl: 'https://tv.cztv.com/vplay/717862.html',
-        videoImage: 'http://i05.cztv.com/cztv/vms/2019/10/07/8f00f140b7894ac1813fc82dd78c7519/9_640_400.jpg',
-        videoAblumId: '1933',
-        videoChannelId: '11',
-*/
+/**
+ * @description: 打开点播页
+ * @param {Object} playOptions
+ *  {
+      videoId: '717862',
+      videoTitle: '《2019中国好声音》第15期：鸟巢总决赛  四大战队严正以待',
+      videoShareUrl: 'https://tv.cztv.com/vplay/717862.html',
+      videoImage: 'http://i05.cztv.com/cztv/vms/2019/10/07/8f00f140b7894ac1813fc82dd78c7519/9_640_400.jpg',
+      videoAblumId: '1933',
+      videoChannelId: '11'
+    }
+ * @return {*}
+ */
 CztvApi.prototype.vodPlay = function (playOptions) {
-  console.log('vodPlay')
+  if (this.debug) console.log('vodPlay')
+
   playOptions != null && setupWebViewJavascriptBridge(function (bridge) {
     bridge.callHandler('CZTVJsdk.vodPlay', playOptions)
   })
 }
 
-/*
-    打开会员页
-*/
+/**
+ * @description: 打开会员页
+ * @param {*} resCallback
+ * @return {*}
+ */
 CztvApi.prototype.pushVipPage = function (resCallback) {
-  console.log('pushVipPage')
+  const debug = this.debug
+  if (debug) console.log('pushVipPage')
+
   setupWebViewJavascriptBridge(function (bridge) {
     bridge.callHandler('CZTVJsdk.pushVipPage', null, function (res) {
-      console.log('打开会员页：' + res)
+      if (debug) console.log('打开会员页：', res)
+
       if (resCallback) {
         resCallback(res)
       }
@@ -177,15 +216,20 @@ CztvApi.prototype.pushVipPage = function (resCallback) {
   })
 }
 
-/*
-    登录
-    未登录则返回''
-*/
+/**
+ * @description: 唤起登录页，未登录则返回''，登录成功code: 200，取消登录code: 401
+ * 20220817直接关闭登录页，安卓端有code返回，ios无任何返回
+ * @param {*} resCallback
+ * @return {*}
+ */
 CztvApi.prototype.login = function (resCallback) {
-  console.log('pushLoginPage')
+  const debug = this.debug
+  if (debug) console.log('pushLoginPage')
+
   setupWebViewJavascriptBridge(function (bridge) {
     bridge.callHandler('CZTVJsdk.pushLoginPage', null, function (res) {
-      console.log('登录：' + res)
+      if (debug) console.log('登录：', res)
+
       if (resCallback) {
         resCallback(res)
       }
@@ -193,15 +237,19 @@ CztvApi.prototype.login = function (resCallback) {
   })
 }
 
-/*
-    获取用户信息
-    老接口，未登录会唤起登录页
-*/
+/**
+ * @description: 获取用户信息，未登录会唤起登录页（老接口）
+ * @param {*} resCallback
+ * @return {*}
+ */
 CztvApi.prototype.getUserInfo = function (resCallback) {
-  console.log('getUserInfo')
+  const debug = this.debug
+  if (debug) console.log('getUserInfo')
+
   setupWebViewJavascriptBridge(function (bridge) {
     bridge.callHandler('CZTVJsdk.getUserInfo', null, function (res) {
-      console.log('旧用户信息：' + res)
+      if (debug) console.log('旧用户信息：', res)
+
       if (resCallback) {
         resCallback(res)
       }
@@ -209,15 +257,19 @@ CztvApi.prototype.getUserInfo = function (resCallback) {
   })
 }
 
-/*
-    获取用户信息
-    新接口，登录成功code：200
-*/
+/**
+ * @description: 仅获取用户信息（新接口），可用来判断是否已登录，未登录则返回''
+ * @param {*} resCallback
+ * @return {*} 
+ */
 CztvApi.prototype.userInfo = function (resCallback) {
-  console.log('userInfo')
+  const debug = this.debug
+  if (debug) console.log('userInfo')
+
   setupWebViewJavascriptBridge(function (bridge) {
     bridge.callHandler('CZTVJsdk.userInfo', null, function (res) {
-      console.log('新用户信息：' + res)
+      if (debug) console.log('新用户信息：', res)
+
       if (resCallback) {
         resCallback(res)
       }
@@ -225,15 +277,187 @@ CztvApi.prototype.userInfo = function (resCallback) {
   })
 }
 
-/*
-    修改标题
-    title:(string) 标题名称;
-*/
+/**
+ * @description: 修改标题
+ * @param {String} title
+ * @param {*} resCallback
+ * @return {*}
+ */
 CztvApi.prototype.refreshTitle = function (title, resCallback) {
-  console.log('refreshTitle')
+  const debug = this.debug
+  if (debug) console.log('refreshTitle')
+
   setupWebViewJavascriptBridge(function (bridge) {
     bridge.callHandler('CZTVJsdk.refreshTitle', title, function (res) {
-      console.log(res)
+      if (debug) console.log('refreshTitle:', res)
+
+      if (resCallback) {
+        resCallback(res)
+      }
+    })
+  })
+}
+
+/**
+ * @description: 分享平台配置，配置右上角调起的分享面板中的图标和方法，后续可扩展
+ * @param {Object} addSharePlatformdata 文档入参为JSON，实际测试无效，请以Object格式传入
+ *  {
+ *    // 桌面快捷方式
+ *    "shortcut": {
+ *      "shortLabel": "标题",
+ *      "icon": "桌面icon",
+ *      "url": "下次打开显示的链接",
+ *      "isPinShortcutSupported": true // 是否支持快捷方式
+ *    }
+ *  }
+ * @param {*} resCallback
+ * @return {*}
+ */
+CztvApi.prototype.addSharePlatform = function (addSharePlatformdata, resCallback) {
+  const debug = this.debug
+  if (debug) console.log('addSharePlatform')
+
+  setupWebViewJavascriptBridge(function (bridge) {
+    bridge.callHandler('CZTVJsdk.addSharePlatform', addSharePlatformdata, function (res) {
+      if (debug) console.log('右上角面板配置：', res)
+
+      if (resCallback) {
+        resCallback(res)
+      }
+    })
+  })
+}
+
+/**
+ * @description: 查询客户端是否支持指定的JS方法
+ * @param {Object} apiList 查询的api数组对象
+ *  {
+ *    "apiList"：[ 
+ *      'CZTVJsdk.showShareButton', 
+ *      'CZTVJsdk.share' 
+ *    ]
+ *  }
+ * @param {*} resCallback
+ * @return {*} res 以键值对的形式告知前端结果
+ * {
+ *    "code": 200,
+ *    "data": {
+ *      "checkResult": {
+ *        "CZTVJsdk.showShareButton": true,
+ *        "CZTVJsdk.share ": false
+ *      }
+ *    }
+ * }
+ */
+CztvApi.prototype.checkJsApi = function (apiList, resCallback) {
+  const debug = this.debug
+  if (debug) console.log('checkJsApi')
+
+  setupWebViewJavascriptBridge(function (bridge) {
+    bridge.callHandler('CZTVJsdk.checkJsApi', apiList, function (res) {
+      if (debug) console.log('当前版本客户端是否支持指定CztvApi:', res)
+
+      if (resCallback) {
+        resCallback(res)
+      }
+    })
+  })
+}
+
+/**
+ * @description: 图片缩放预览
+ * @param {Object} previewImagedata
+ *  {
+ *    "currentIndex"：0, // 当前预览下标
+ *    "imgeData": "src,src" // 网络地址，逗号分隔
+ *  }
+ * @param {*} resCallback
+ * @return {*}
+ */
+CztvApi.prototype.previewImage = function (previewImagedata, resCallback) {
+  const debug = this.debug
+  if (debug) console.log('previewImage')
+
+  setupWebViewJavascriptBridge(function (bridge) {
+    bridge.callHandler('CZTVJsdk.previewImage', previewImagedata, function (res) {
+      if (debug) console.log('图片缩放预览：', res)
+
+      if (resCallback) {
+        resCallback(res)
+      }
+    })
+  })
+}
+
+/**
+ * @description: 获取网络状态
+ * 调用此方法后客户端会注册网络广播进行网络监听，网络发生变更后会再次通过 callback回调给前端
+ * @param {*} resCallback
+ * @return {*} res
+ * {
+ *    "netType": "网络状态" // wifi、mobile、unknown
+ * }
+ */
+CztvApi.prototype.getNetworkType = function (resCallback) {
+  const debug = this.debug
+  if (debug) console.log('getNetworkType')
+
+  setupWebViewJavascriptBridge(function (bridge) {
+    bridge.callHandler('CZTVJsdk.getNetworkType', null, function (res) {
+      if (debug) console.log('获取网络状态：', res)
+
+      if (resCallback) {
+        resCallback(res)
+      }
+    })
+  })
+}
+
+/**
+ * @description: 调起app页面
+ * @param {Object} openPagedata
+ * {
+ *    "resource_type": 1, //跳转类型
+ *    "resource_body": "id" //页面加载所用到的id
+ * }
+ * @param {*} resCallback
+ * @return {*}
+ */
+CztvApi.prototype.openPage = function (openPagedata, resCallback) {
+  const debug = this.debug
+  if (debug) console.log('openPage')
+
+  setupWebViewJavascriptBridge(function (bridge) {
+    bridge.callHandler('CZTVJsdk.openPage', openPagedata, function (res) {
+      if (debug) console.log('调起app页面：', res)
+
+      if (resCallback) {
+        resCallback(res)
+      }
+    })
+  })
+}
+
+/**
+ * @description: 配置胶囊样式UI
+ * @param {Object} configureCapsuledata 文档入参为JSON，实际测试无效，请以Object格式传入
+ * {
+ *    "showCapsule": true, // 显示胶囊，否则用老样式
+ *    "capsuleBgColor": "#ffffff", // 胶囊背景颜色
+ *    "capsuleButtonColor": "#000000", // 胶囊按钮颜色
+ *    "icon": "https://ohudong.cztv.com/1/261597/images/tvLogo.png" // 左上角icon
+ * }
+ * @param {*} resCallback
+ * @return {*}
+ */
+CztvApi.prototype.configureCapsule = function (configureCapsuledata, resCallback) {
+  const debug = this.debug
+  if (debug) console.log('configureCapsule')
+
+  setupWebViewJavascriptBridge(function (bridge) {
+    bridge.callHandler('CZTVJsdk.configureCapsule', configureCapsuledata, function (res) {
+      if (debug) console.log('配置胶囊样式UI：', res)
+
       if (resCallback) {
         resCallback(res)
       }
